816cc86f4dc18fc1295676ed250614cb28694489
SpectreSpect <spectrefromthefuture@gmail.com>
2021-12-08 19:41:59 +0400

    Made a output

--- BasicsOfProgramming-Lab13-StringsAndModules/Source.cpp
+++ BasicsOfProgramming-Lab13-StringsAndModules/Source.cpp
@@ -1,9 +1,99 @@
 #include <stdio.h>
-#include <iostream>
 #include "gamma.h"
+
+#define MAX_SYMBOLS 256
 
 int main()
 {
+	printf("Choose what you want the program to do\n");
+	printf("	1. gstrlen\n");
+	printf("	2. gstrcpy\n");
+	printf("	3. gstrcat\n");
+	printf("	4. strcpm\n");
+	printf("	5. gstrchr\n");
+	printf("	6. gstrstr\n");
+	printf("	7. Quit the program\n");
 
+	int choice = 0;
+	scanf_s("%d", &choice);
+	
+	int loop = 1;
+	while (loop)
+	{
+		switch (choice)
+		{
+		case 1:
+		{
+			char str1[MAX_SYMBOLS];
+			printf("Enter string:");
+			scanf_s(" %[^\t\n]s", str1, MAX_SYMBOLS);
+			printf_s("Symbols count:%d\n", gstrlen(str1));
+			break;
+		}
+		case 2:
+		{
+			char str1[MAX_SYMBOLS];
+			char str2[MAX_SYMBOLS];
+			printf("Enter first string:");
+			scanf_s(" %[^\t\n]s", str1, MAX_SYMBOLS);
+			printf("Enter second string:");
+			scanf_s(" %[^\t\n]s", str2, MAX_SYMBOLS);
+			printf_s("Result:%s", gstrcpy(str1, str2));
+			break;
+		}
+		case 3:
+		{
+			char str1[MAX_SYMBOLS];
+			char str2[MAX_SYMBOLS];
+			printf("Enter first string:");
+			scanf_s(" %[^\t\n]s", str1, MAX_SYMBOLS);
+			printf("Enter second string:");
+			scanf_s(" %[^\t\n]s", str2, MAX_SYMBOLS);
+			printf_s("Result:%s", gstrcat(str1, str2));
+			break;
+		}
+		case 4:
+		{
+			char str1[MAX_SYMBOLS];
+			char str2[MAX_SYMBOLS];
+			printf("Enter first string:");
+			scanf_s(" %[^\t\n]s", str1, MAX_SYMBOLS);
+			printf("Enter second string:");
+			scanf_s(" %[^\t\n]s", str2, MAX_SYMBOLS);
+			printf_s("Result:%d", strcpm(str1, str2));
+			break;
+		}
+		case 5:
+		{
+			char str1[MAX_SYMBOLS];
+			int symbol = 0;
+			printf("Enter string:");
+			scanf_s(" %[^\t\n]s", str1, MAX_SYMBOLS);
+			printf("Enter char:");
+			scanf_s(" %c", (char*)&symbol, MAX_SYMBOLS);
+			printf_s("The first occurence of symbol is placed at %d", gstrlen(str1) - gstrlen(gstrchr(str1, symbol)) + 1);
+			break;
+		}
+		case 6:
+		{
+			char str1[MAX_SYMBOLS];
+			char str2[MAX_SYMBOLS];
+			printf("Enter first string:");
+			scanf_s(" %[^\t\n]s", str1, MAX_SYMBOLS);
+			printf("Enter second string:");
+			scanf_s(" %[^\t\n]s", str2, MAX_SYMBOLS);
+			int strlen1 = gstrlen(str1);
+			int strlen2 = gstrlen(gstrstr(str1, str2));
+			printf_s("The first occurence of second string is placed at %d", strlen1 - strlen2 + 1);
+			break;
+		}
+		case 7:
+		{
+			loop = 0;
+			break;
+		}
+	}
+
+	}
 	return 0;
 }
